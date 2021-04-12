e2000a7f13aed941cf98427bc9612d319b3fbe2c
Luis Alvarez <lealvarezp@unac.edu.co>
2021-04-12 05:00:44 -0500

    merging schemas

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,6 +1,11 @@
-import { makeExecutableSchema } from "graphql-tools";
 import "graphql-import-node";
-import rootSchema from "./schema/schema.graphql";
-export const schema = makeExecutableSchema({
-  typeDefs: [rootSchema],
+import path from "path";
+import { fileLoader, mergeTypes } from "merge-graphql-schemas";
+// const fileLoader = mergeGraphqlSchemas.fileLoader;
+// const mergeTypes = mergeGraphqlSchemas.mergeTypes;
+
+const typesArray = fileLoader(path.join(__dirname, "/schema/*.*"), {
+  recursive: true,
 });
+
+export const types = mergeTypes(typesArray, { all: true });
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import express from "express";
+import { buildSchema } from "graphql";
 import cors from "cors";
 import { ApolloServer } from "apollo-server-express";
-import { schema } from "./graphql";
+import { types } from "./graphql";
 
 const app = express();
 app.use(cors());
-
+const schema = buildSchema(types);
 const server = new ApolloServer({
   introspection: true,
   playground: true,
