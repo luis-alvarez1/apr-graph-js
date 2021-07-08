11dac514b466de157ec3c6573fbb81f3b4280eae
Luis Alvarez <lealvarezp@unac.edu.co>
2021-07-08 17:59:15 -0500

    updating shit

--- src/util/helpers/credit/creditHelpers.js
+++ src/util/helpers/credit/creditHelpers.js
@@ -1,5 +1,5 @@
-// eslint-disable-next-line import/prefer-default-export
 export const getFeeValue = (creditValue, feesNumber) =>
   creditValue / feesNumber;
 
-export const isExpired = (credit) => Date.now() > credit.nextFeeExpiration;
+export const isExpired = (credit) =>
+  Date.now() > credit.nextFeeExpiration;
